f4f483fedd4451c506787d6f04897170ea2c6176
bugRanger <departmentofjusticeevil@gmail.com>
2023-07-26 21:03:52 +0700

    Add application stop on signal ^C

--- main.go
+++ main.go
@@ -4,22 +4,24 @@ import (
 	"flag"
 	"log"
 	"main/src/router"
-	"time"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
-
 	addressFlag := flag.String("l", "127.0.0.1:7", "a string as `127.0.0.1:7`")
 	flag.Parse()
 
 	router := &router.EchoRouter{}
+	defer router.Close()
 
 	err := router.Open(*addressFlag)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	time.Sleep(30 * time.Second)
-
-	router.Close()
+	chSig := make(chan os.Signal)
+	signal.Notify(chSig, syscall.SIGINT, syscall.SIGTERM)
+	log.Println("Signal: ", <-chSig)
 }
